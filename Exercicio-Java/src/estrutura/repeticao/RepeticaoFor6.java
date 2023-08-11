19efb0402606051eb520be360a8f43e6a5202bad
gioalvxs <gioalvxs@gmail.com>
2023-08-11 20:27:45 -0300

    novos exercicios

---
+++ Exercicio-Java/src/estrutura/repeticao/RepeticaoFor6.java
@@ -0,0 +1,25 @@
+package estrutura.repeticao;
+
+import java.util.Scanner;
+
+public class RepeticaoFor6 {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int n;
+
+        System.out.println("Digite um número: ");
+        n = input.nextInt();
+
+        for(int i = 0; i < n ; i++){
+
+            if (n % i == 0){
+
+                System.out.println(i);
+
+            }
+            input.close();
+        }
+    }
+}
---
+++ Exercicio-Java/src/estrutura/repeticao/RepeticaoFor7.java
@@ -0,0 +1,28 @@
+package estrutura.repeticao;
+
+import java.util.Scanner;
+
+public class RepeticaoFor7 {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int n;
+        int primeiro;
+        int segundo;
+        int terceiro;
+
+        System.out.println("Informe um número: ");
+        n = input.nextInt();
+
+        for( int i = 0 ; i <= n ; i++ ){
+
+              primeiro = i;
+              segundo = i * i;
+              terceiro = i * i * i;
+
+            System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+
+        }
+    }
+}
